b904b44707f39b5696929f58965e1e7232f25e33
Mohd Faiz Hasim <faizhasim@gmail.com>
2019-09-11 16:46:13 +0800

    More changes

--- apps/photos.sh
+++ apps/photos.sh
@@ -5,22 +5,22 @@
 ###############################################################################
 
 # Startup (with iCloud Photos selected)
-defaults write com.apple.Photos IPXDefaultDidPromoteiCloudPhotosInGettingStarted -bool true
-defaults write com.apple.Photos IPXDefaultHasBeenLaunched -bool true
-defaults write com.apple.Photos IPXDefaultHasChosenToEnableiCloudPhotosInGettingStarted = 1;
+#defaults write com.apple.Photos IPXDefaultDidPromoteiCloudPhotosInGettingStarted -bool true
+#defaults write com.apple.Photos IPXDefaultHasBeenLaunched -bool true
+#defaults write com.apple.Photos IPXDefaultHasChosenToEnableiCloudPhotosInGettingStarted = 1;
 
 # Summarize photos
-# You can choose compast, summarized views for Collections and Years
-defaults write com.apple.Photos IPXDefaultPhotosSummarizePhotos -bool true
+## You can choose compast, summarized views for Collections and Years
+#defaults write com.apple.Photos IPXDefaultPhotosSummarizePhotos -bool true
 
-# Copy items to the Photos library
-# Only items copied to the library will upload to iCloud Photo library (when disabled)
-defaults write com.apple.Photos IPXDefaultImportUseReferencedImport -bool false
+## Copy items to the Photos library
+## Only items copied to the library will upload to iCloud Photo library (when disabled)
+#defaults write com.apple.Photos IPXDefaultImportUseReferencedImport -bool false
 
-# Include location information for published items
-defaults write com.apple.Photos IPXDefaultPlacesPublishPlaceInfo -bool false
+## Include location information for published items
+#defaults write com.apple.Photos IPXDefaultPlacesPublishPlaceInfo -bool false
 
-# Print Products Store
-# Choose a store based on your billing address.
-defaults write com.apple.Photos KHStoreCountryExplicitlySetByUserKey -bool true
-defaults write com.apple.Photos KHUserPreferredStoreCountryKey -string 'US'
+## Print Products Store
+## Choose a store based on your billing address.
+#defaults write com.apple.Photos KHStoreCountryExplicitlySetByUserKey -bool true
+#defaults write com.apple.Photos KHUserPreferredStoreCountryKey -string 'US'
